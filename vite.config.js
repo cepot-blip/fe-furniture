b0b04045b630a0e796f0925372e0052b86da6001
cepot-blip <85933775+cepot-blip@users.noreply.github.com>
2025-01-07 11:26:08 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+const react = require('@vitejs/plugin-react');
+const { defineConfig } = require('vite');
 
-// https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
 });
