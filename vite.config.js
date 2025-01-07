807b27335ff9c0964dbfb35b940f4b921ab58675
cepot-blip <85933775+cepot-blip@users.noreply.github.com>
2025-01-07 11:34:37 +0700

    fix bug issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020',
     },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+    origin: 'http://0.0.0.0:5173',
   },
   build: {
     outDir: 'dist',
