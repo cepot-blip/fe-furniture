00cb67cc9791da1a808afbbda8295dd29a1ba66c
Noufal Trianto <noufaltrianto@gmail.com>
2025-01-08 21:54:38 +0700

    refactor:refactor code cartItem.length

--- src/components/Fragments/Navbar/Navbar.jsx
+++ src/components/Fragments/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar({ onCartClick }) {
                   onClick={handleCrartClick}
                 />
                 {cartItem.length > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
+                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5">
                     {cartItem.length}
                   </span>
                 )}
